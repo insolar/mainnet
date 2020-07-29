694864fbd3a4d6f355e80bc93a1ebc3bce60d089
Alexey Zhunin <zhunin.a.u@gmail.com>
2020-07-29 15:30:04 +0300

    MN-636: add method `account.transferToDeposit` (#42)
    * MN-636: add method `account.transferToDeposit`
    
    * MN-636: rename `TransferToDeposit` to `ReallocateToDeposit`
    
    * MN-636: add balance checks to functest

--- cmd/insolard/api.go
+++ cmd/insolard/api.go
@@ -65,6 +65,7 @@ func initAPIOptions() (api.Options, error) {
 		"deposit.migration":          true,
 		"deposit.createFund":         true,
 		"member.getBalance":          true,
+		"account.transferToDeposit":  true,
 		"deposit.transferToDeposit":  true,
 	}
 	contractMethods := map[string]bool{
